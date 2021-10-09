126173df78504409753556e8a027e28284144e57
Jakub Gluszek <j.gluszek@ien.gda.pl>
2021-10-09 22:06:07 +0200

    Downgrade to .NET Framework 4.7.2

--- Lab1/OpenClosedPrinciple/Program.cs
+++ Lab1/OpenClosedPrinciple/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Add IShape interface, and implement in Circle as well as in Square so that ShapeDrawer could meet the open closed principle based on:
+// https://pierresimondelaplace.gitlab.io/wsb-zaawansowane-techniki-obiektowe/WSB_ZaawansowaneTechnikiObiektowe.pdf
 
 namespace OpenClosedPrinciple
 {
