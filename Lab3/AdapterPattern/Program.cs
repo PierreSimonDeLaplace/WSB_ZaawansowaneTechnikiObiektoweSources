e3c9652e7b53d65618a57dcea381c723d1ca79e6
Jakub Gluszek <j.gluszek@ien.gda.pl>
2021-10-26 08:28:58 +0200

    Add AdapterPattern startup project.

---
+++ Lab3/AdapterPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//using NLog;
+
+// Add the NLog package and complete the tasks based on:
+// https://pierresimondelaplace.gitlab.io/wsb-zaawansowane-techniki-obiektowe/WSB_ZaawansowaneTechnikiObiektowe.pdf
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        //private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            //Logger.Info("Hello world");
+            //Logger.Error("Goodbye cruel world!");
+
+            Console.ReadLine();
+        }
+    }
+}
